acff6fb9717fd048392f5a93cf16bde3e8e88a78
aroran1 <nidhigulati2001@gmail.com>
2021-01-13 17:48:44 +0000

    added nodemon

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const validator = require('validator');
 const chalk = require('chalk');
 const getNotes = require('./notes');
 
+const greenMsg = chalk.green.inverse.bold('Success!');
+const blueRedMsg = chalk.blue.bgRed.bold('Error!');
+
 // Challenge 1
 fs.writeFileSync('./generated-files/notes.txt', 'This file is created by node fs!');
 // fs.writeFileSync('notes.txt', 'Created by- Nidhi Arora'); Overwites the first line
@@ -12,5 +15,5 @@ fs.appendFileSync('./generated-files/notes.txt', ' Created by- Nidhi Arora');
 const msg = getNotes();
 console.log(msg);
 console.log(validator.isEmail("dfdj.sdjhfsd@sdhfhs.com"));
-console.log(chalk.green('Success!'));
-console.log(chalk.blue.bgRed.bold('Hello world!'));
+console.log(greenMsg);
+console.log(blueRedMsg);
