b9f25670eecd213fcf7fb7a67466581704f99a4c
aroran1 <nidhigulati2001@gmail.com>
2021-01-13 17:08:30 +0000

    added notes and module.exports

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const getNotes = require('./notes');
 
+// Challenge 1
 fs.writeFileSync('./generated-files/notes.txt', 'This file is created by node fs!');
-
-
 // fs.writeFileSync('notes.txt', 'Created by- Nidhi Arora'); Overwites the first line
-
 // to append use appendFileSync method
-
 fs.appendFileSync('./generated-files/notes.txt', ' Created by- Nidhi Arora');
+
+const msg = getNotes();
+console.log(msg);
