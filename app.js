c8edde70c1cf8658ea0d26c55cbb64302c4507e7
aroran1 <nidhigulati2001@gmail.com>
2021-01-13 16:51:00 +0000

    moved generated files to a folder

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'This file is created by node fs!');
+fs.writeFileSync('./generated-files/notes.txt', 'This file is created by node fs!');
 
 
 // fs.writeFileSync('notes.txt', 'Created by- Nidhi Arora'); Overwites the first line
 
 // to append use appendFileSync method
 
-fs.appendFileSync('notes.txt', ' Created by- Nidhi Arora');
+fs.appendFileSync('./generated-files/notes.txt', ' Created by- Nidhi Arora');
