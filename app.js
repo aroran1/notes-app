e8df9a6445836788a1166c5bf6f1792f259e9eff
aroran1 <nidhigulati2001@gmail.com>
2021-01-15 16:07:07 +0000

    added yargs command and data

--- app.js
+++ app.js
@@ -30,8 +30,21 @@ yargs.version('1.1.0');
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function() {
-    console.log('Adding a new notes!');
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    console.log(`Title: ${argv.title}`);
+    console.log(`Body: ${argv.body}`);
   }
 })
 
@@ -62,4 +75,5 @@ yargs.command({
   }
 })
 
-console.log(yargs.argv);
+yargs.parse();
+// console.log(yargs.argv);
