1fd15df03789aa8042514d2db9dfb3da0000c962
aroran1 <nidhigulati2001@gmail.com>
2021-01-15 16:20:08 +0000

    dynamically create json file with parsed data

---
+++ playground/1-json.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const book = {
+  title: "Ego is the Enemy",
+  author: "Ryan Holiday"
+}
+
+// JSON.stringify method take in a javascript object and return a json object
+const bookJson = JSON.stringify(book); 
+// methods like bookJson.title will return undefined as its not a javascript object anymore
+console.log(bookJson);
+
+fs.writeFileSync('1-json.json', bookJson); // creates the json file with the passed data
+
+// JSON.parse method take in a json object and return a javascript object
+const parsedData = JSON.parse(bookJson);
+console.log(parsedData);
+console.log(parsedData.author);
